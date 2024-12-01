5bd2362319756e67671fd85322f18471c985e453
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-01 21:57:51 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 function userScroll() {
     const navbar = document.querySelector('.navbar');
 
-    if (window.scrollY > 50 ) {
-        navbar.classList.add('bg-dark');
-    }
-    else {
-        navbar.classList.remove('bg-dark');
-    }
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50 ) {
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
+        }
+        else {
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-sticky');
+        }
+    });
 }
 
-document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', userScroll); 
