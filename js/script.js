4e1d94e4fd478bb9a64c780e2cb9974d60794761
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-17 09:45:01 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ function typeText(text, typingElement, typingDelay) {
 }
 
 document.addEventListener('DOMContentLoaded', runTypingEffect);
+
+//  main.js
+ 
+// POST request using fetch()
+fetch("http://ooustaffrequest.us-east-2.elasticbeanstalk.com/oouweb/staffs/login", {
+    
+    // Adding method type
+    method: "POST",
+    
+    // Adding body or contents to send
+    body: JSON.stringify({
+        email: "chris@chris1.com",
+        password: "christopher"
+    }),
+    
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+
+// Converting to JSON
+.then(response => {
+
+    console.log('headers: ' + response.headers);
+    console.log('status: ' + response.status);
+
+    response.json()
+})
+
+// Displaying results to console
+.then(json => console.log(json));
